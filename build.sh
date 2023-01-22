9c1f6fde790abfc9e98d2a702005f9ffb2f0be3e
asterlinux <sahan.user@gmail.com>
2023-01-22 09:53:01 +0530

    Kernel

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@
 
 
 sudo apt install tar wget
+sudo apt-get build-dep linux linux-image-$(uname -r)
+sudo apt-get install libncurses-dev gawk flex bison openssl libssl-dev dkms libelf-dev libudev-dev libpci-dev libiberty-dev autoconf llvm
 wget https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.1.7.tar.xz
 tar -xf linux-6.1.7.tar.xz
 sudo chown -R $USER:$USER linux-6.1.7
